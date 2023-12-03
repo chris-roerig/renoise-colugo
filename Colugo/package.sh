884ac216bc7f2009e0537bb16534ed5d856fc8c6
Chris <croerig@gmail.com>
2023-12-03 00:04:13 -0500

    fixed package.sh - forgot to include the python script

--- Colugo/package.sh
+++ Colugo/package.sh
@@ -3,4 +3,4 @@
 ID=$(sed -n 's:.*<Id>\(.*\)</Id>.*:\1:p' manifest.xml)
 echo Creating "$ID.xrnx"
 rm -f *.xrnx
-zip -vr "$ID.xrnx" *.lua manifest.xml
+zip -vr "$ID.xrnx" *.lua yt-downloader.py manifest.xml
